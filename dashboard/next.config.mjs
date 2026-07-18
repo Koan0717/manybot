ea6b7ee4155b7fe959c258a9b0762cd6897b642c
Koan0717 <kakijun0717@gmail.com>
2026-07-19 02:18:04 +0900

    Update next.config.mjs

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://discord.com https://*.discord.com;",
+          }
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
